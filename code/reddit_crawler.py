e68a55edb6f2865a45129325cfb3ccba9e49f4d1
Arka Sadhu <ark.sadhu2904@gmail.com>
2018-09-14 13:39:28 -0700

    Setting up travis and pylint

---
+++ code/reddit_crawler.py
@@ -0,0 +1,11 @@
+"""
+Author: Arka Sadhu
+This is the main file to crawl reddit and save the extracted information.
+"""
+
+
+if __name__ == '__main__':
+    """
+    Main function
+    """
+    print('Hello')
